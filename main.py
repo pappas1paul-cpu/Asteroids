d10dec5bb45c69d2857a3427d03a7d744c90fac3
ppappas <pavlos127@gmail.com>
2026-03-25 22:18:38 +0200

    Added screen dimensions

---
+++ main.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3.13
+import pygame
+from constants import SCREEN_WIDTH, SCREEN_HEIGHT
+
+def main():
+    print(f"Starting Asteroids with pygame version: {pygame.version.ver}")
+    print("Screen width:",SCREEN_WIDTH)
+    print("Screen height:",SCREEN_HEIGHT)
+
+if __name__ == "__main__":
+    main()
