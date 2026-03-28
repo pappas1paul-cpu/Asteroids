f650b079780bb6fb1c45127db0819f4779686f10
ppappas <pavlos127@gmail.com>
2026-03-28 22:08:13 +0200

    fixed the screen issue

--- main.py
+++ main.py
@@ -2,6 +2,7 @@
 import pygame
 from constants import SCREEN_WIDTH, SCREEN_HEIGHT
 from logger import log_state
+from player import Player
 
 def main():
     print(f"Starting Asteroids with pygame version: {pygame.version.ver}")
@@ -12,15 +13,19 @@ def main():
     clock = pygame.time.Clock()
     dt=0
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
     while True:
         log_state()
         for event in pygame.event.get():
             log_state()
             pass
         screen.fill("black")
+        player.draw(screen)
         pygame.display.flip()
         dt = clock.tick(60) / 1000
-        print(dt)
+        # print(dt)
+
+
 
 if __name__ == "__main__":
     main()
