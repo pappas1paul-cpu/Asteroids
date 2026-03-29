87fee2b1308db078e18746cf6adb1958282504f2
ppappas <pavlos127@gmail.com>
2026-03-29 19:57:55 +0300

    Spaceship complete instead completed

--- main.py
+++ main.py
@@ -8,22 +8,26 @@ def main():
     print(f"Starting Asteroids with pygame version: {pygame.version.ver}")
     print("Screen width:",SCREEN_WIDTH)
     print("Screen height:",SCREEN_HEIGHT)
-   
+    
     pygame.init()
     clock = pygame.time.Clock()
     dt=0
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+    Player.containers = (updatable, drawable)
     player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
     while True:
-        log_state()
         for event in pygame.event.get():
-            log_state()
-            pass
+            if event.type == pygame.QUIT:
+                return
         screen.fill("black")
-        player.draw(screen)
+        updatable.update(dt)
+        log_state()
+        for draw in drawable:
+            draw.draw(screen)
         pygame.display.flip()
-        dt = clock.tick(60) / 1000
-        # print(dt)
+        dt = clock.tick(60) / 1000  
 
 
 
